cf867a07fb2ee4326f4d817c5feb4c3fc5fb0f9b
Grek Maria <grekmariaa@gmail.com>
2023-03-18 12:49:08 +0200

    Update UserList.jsx

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -1,9 +1,9 @@
 import userData from "../../db/userData.json";
 import css from "./UserList.module.css"
-import { UserItems } from "components/UserItems/UserItems"
+import { UserItems } from "../UserItems/UserItems"
 
 export const UserList = () => {
-    const users = userData.users
+  const users = userData.users
 
   return <div className={css.UserList}>
     {users.map(user => <UserItems key={user.id} user={user} />)}
